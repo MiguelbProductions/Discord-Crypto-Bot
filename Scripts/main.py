c1129622b8aa56e4d7f91a08d903bc59e564ba61
Miguel Brandão <miguellbproductions@gmail.com>
2022-10-27 23:36:02 +0100

    Create helpme command
    When client use '!helpme' I show a list of commands of bot

--- Scripts/main.py
+++ Scripts/main.py
@@ -5,12 +5,17 @@ bot = commands.Bot(command_prefix='!',intents=discord.Intents.all())
 
 @bot.event
 async def on_ready():
-  print(f"Hi my name is {bot.user.name}. Hi ready!")
+  print(f"Hi my name is {bot.user.name}. I'm ready!")
   
 @bot.command(name="hi")
 async def send_hi(ctx):
     name = ctx.author.name
 
     await ctx.send(f"Hi {name}!")
+
+@bot.command(name="helpme")
+async def send_help(ctx):
+    await ctx.send("""List of commands:
+    - help""")
 
 bot.run("OTU4NzI1OTY5NDU0MTk4ODA1.G7vpzY.7NVdp0__maj78j8dutHDvH0cfVAVzhhNMIfLn8")
