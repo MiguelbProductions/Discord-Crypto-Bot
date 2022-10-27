6f6294062bb13cd2f749b490a22a5785ec793276
Miguel Brandão <miguellbproductions@gmail.com>
2022-10-27 23:17:03 +0100

    Send Hi comand
    I create send hi comand when client use command "!hi"

--- Scripts/main.py
+++ Scripts/main.py
@@ -7,4 +7,10 @@ bot = commands.Bot(command_prefix='!',intents=discord.Intents.all())
 async def on_ready():
   print(f"Hi my name is {bot.user.name}. Hi ready!")
   
+@bot.command(name="hi")
+async def send_hi(ctx):
+    name = ctx.author.name
+
+    await ctx.send(f"Hi {name}!")
+
 bot.run("OTU4NzI1OTY5NDU0MTk4ODA1.G7vpzY.7NVdp0__maj78j8dutHDvH0cfVAVzhhNMIfLn8")
